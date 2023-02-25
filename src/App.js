63a8e8c4720faa9bc3bb257b32a2e6f92334fa45
MaximilianoGonzalez <maxi.gonzalez096@gmail.com>
2023-02-25 20:32:45 -0300

    primera pre entrega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Footer from "./Components/Footer/Footer.jsx";
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer.jsx";
-import Navbar from "./Components/Navbar/Navbar.jsx"
+import Navbar from "./Components/Navbar/Navbar.jsx";
 import Productos from "./Components/Productos/Productos.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ItemListContainer />
+      <ItemListContainer greeting={"Hola"} />
       <Productos />
       <Footer />
     </div>
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,9 @@
-
-
-const ItemListContainer = () => {
+const ItemListContainer = ({ greeting }) => {
   return (
-    <div>ItemListContainer</div>
-  )
-}
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  );
+};
 
-export default ItemListContainer
+export default ItemListContainer;
